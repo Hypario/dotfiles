a9dbf3c9c0f741080670e3c3438fb0db9aefa454
Hypario <fabienduterte@mailfence.com>
2021-01-18 15:43:12 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#/usr/env bash
+
+sudo apt update && sudo apt upgrade
+
+# install vscodium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg
+
+# installing zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sudo apt install fonts-powerline
