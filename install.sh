1520866e3e8c8984172dc508d04dd32a6e7a5866
Fabien <fabienduterte@orange.fr>
2021-01-29 11:48:26 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,32 +19,45 @@ if [ $choice == "y" ]; then
 		sudo apt install git -y
 	fi
 
-	# copy zshrc
-	echo "copy zshrc"
-	if [ -f $HOME/.zshrc ]; then
-		rm $HOME/.zshrc
-	fi
-	if [ -f $HOME/.zshrc.pre-oh-my-zsh ]; then
-		rm $HOME/.zshrc.pre-oh-my-zsh
-	fi
-	cp $HOME/dotfiles/.zshrc $HOME/.zshrc
-
 	# install vscodium
-	wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg
+	read -p "Install vscodium ? (y/n) " answer_vscodium
+	if [ $answer_vscodium == "y" ]; then
+		wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg
+	fi
 
 	# install nvm
-	wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+	read -p "Install nvm ? (y/n) " answer_nvm
+	if [ $answer_nvm == "y" ]; then
+		wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+	fi
 
 	# installing oh-my-zsh
-	echo "install oh-my-zsh"
-	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -unattended
+	read -p "Install oh-my-zsh ? (y/n) " answer_omz
+	if [ $answer_omz == "y" ]; then
+		echo "install oh-my-zsh"
+		sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -unattended
+
+		echo "installing font for zsh theme"
+		sudo apt install fonts-powerline
+
+		# copy zshrc
+		echo "copy zshrc"
+		if [ -f $HOME/.zshrc ]; then
+			rm $HOME/.zshrc
+		fi
+
+		if [ -f $HOME/.zshrc.pre-oh-my-zsh ]; then
+			rm $HOME/.zshrc.pre-oh-my-zsh
+		fi
+		cp $HOME/dotfiles/.zshrc $HOME/.zshrc
+	fi
 
 	# installing zsh-autosuggestions
-	echo "install zsh-autossuggestions"
-	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-	echo "installing font for zsh theme"
-	sudo apt install fonts-powerline
+	read -p "Install zsh-autossuggestions ? (y/n) " answer_autosugg
+	if [ $answer_autosugg == "y" ]; then
+		echo "install zsh-autossuggestions"
+		git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	fi
 
 	# install discord
 	TEMP_DEB="$(mktemp)"
